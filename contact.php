cb5491b5380282a705a10e77e93ee6cc997e2b9a
Sefer Tasdemir <sefer.tasdemir@etu.univ-smb.fr>
2024-10-21 21:37:36 +0200

    eng forget + agancement skill + repair portfolio project + new form

--- contact.php
+++ contact.php
@@ -1,12 +1,13 @@
 <?php
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = htmlspecialchars($_POST['firstName']);
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
     $to = "tasdemir.sefer74@gmail.com";
     $subject = "NEW message du portfolio";
-    $body = "Nom: $name\nEmail: $email\nMessage:\n$message";
+    $body = "Prénom: $firstName\nNom: $name\nEmail: $email\nMessage:\n$message";
     $headers = "From: $email";
 
     if (mail($to, $subject, $body, $headers)) {
