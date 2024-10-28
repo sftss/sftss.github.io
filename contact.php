a3a339b4db16a48915c8c2dc956fa53125312afd
Sefer Tasdemir <tasdemirsefer2005@gmail.com>
2024-10-28 18:55:16 +0100

    test contact

--- contact.php
+++ contact.php
@@ -1,46 +1,44 @@
 <?php
+$siteKey = "6LejqG4qAAAAABlIzVJGqdeXY1jM40vidOhODuit";
+$apiKey = "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"; 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
-    $captchaResponse = $_POST['g-recaptcha-response'];
+    $token = $_POST['g-recaptcha-response']; // récupère jeton
 
-    // Clé de site et Clé API de votre projet Google reCAPTCHA Enterprise
-    $siteKey = "6LejqG4qAAAAABlIzVJGqdeXY1jM40vidOhODuit";
-    $apiKey = "6LejqG4qAAAAABlIzVJGqdeXY1jM40vidOhODuit"; // Remplacez par votre clé API
+    $apiUrl = "https://recaptchaenterprise.googleapis.com/v1/projects/portfolio-1730133266651/assessments?key=$apiKey";
 
+    // request.json
     $requestBody = json_encode([
         "event" => [
-            "token" => $captchaResponse,
+            "token" => $token,
             "expectedAction" => "submit",
             "siteKey" => $siteKey
         ]
     ]);
 
-    // URL API reCAPTCHA
-    $url = "https://recaptchaenterprise.googleapis.com/v1/projects/$siteKey/assessments?key=$apiKey";
-
-    // Options de la requête HTTP POST
     $options = [
         "http" => [
-            "header"  => "Content-type: application/json\r\n",
-            "method"  => "POST",
+            "header" => "Content-type: application/json\r\n",
+            "method" => "POST",
             "content" => $requestBody
         ]
     ];
     $context = stream_context_create($options);
-    $response = file_get_contents($url, false, $context);
-    $responseKeys = json_decode($response, true);
+    $response = file_get_contents($apiUrl, false, $context);
+    $responseData = json_decode($response, true);
 
-    // Vérification
-    if ($responseKeys["tokenProperties"]["valid"] && $responseKeys["riskAnalysis"]["score"] > 0.5) {
-        // envoyer email score > 0.5
-        $to = "tasdemir.sefer74@gmail.com";
-        $subject = "Nouveau message du portfolio";
-        $body = "Nom et prénom: $name\nEmail: $email\nMessage:\n$message";
-        $headers = "From: $email";
+    // Vérifier jeton
+    if (isset($responseData['tokenProperties']['valid']) && $responseData['tokenProperties']['valid']) {
+        $score = $responseData['riskAnalysis']['score'];
+
+        // Vérification score
+        if ($score >= 0.5) {
+            $to = "tasdemir.sefer74@gmail.com";
+            $subject = "Nouveau message du portfolio";
+            $body = "Nom et prénom: $name\nEmail: $email\nMessage:\n$message";
+            $headers = "From: $email";
 }
-
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 ?>
