2d89be24c1bf326a78e5486ee9195fa0c9f4fb66
Sefer Tasdemir <tasdemirsefer2005@gmail.com>
2024-11-16 10:18:39 +0100

    change lhome page

--- contact.php
+++ contact.php
@@ -9,11 +9,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
-    $token = $_POST['g-recaptcha-response']; // récupère jeton
+    $token = $_POST['g-recaptcha-response']; //récupère jeton
 
     $apiUrl = "https://recaptchaenterprise.googleapis.com/v1/projects/portfolio-1730133266651/assessments?key=$apiKey";
 
-    // request.json equivalent
+    //request.json équivalent
     $requestBody = json_encode([
         "event" => [
             "token" => $token,
@@ -33,11 +33,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $response = file_get_contents($apiUrl, false, $context);
     $responseData = json_decode($response, true);
 
-    // check jeton
+    //check jeton
     if (isset($responseData['tokenProperties']['valid']) && $responseData['tokenProperties']['valid']) {
         $score = $responseData['riskAnalysis']['score'];
     }
-        // check score
+        //check score
         if ($score >= 0.5) {
             $to = "tasdemir.sefer74@gmail.com";
             $subject = "Nouveau message du portfolio";
