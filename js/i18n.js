62319aaa7166cadd1aaa76dcf67dc6cecb498684
Sefer Tasdemir <tasdemirsefer2005@gmail.com>
2024-11-23 21:13:05 +0100

    trad botanic + galaxy rush

--- js/i18n.js
+++ js/i18n.js
@@ -54,6 +54,28 @@ i18next.init(
               skills: "Skills",
               seeGitHub: "See on GitHub",
               projectsPresentaion: "Project presentation",
+              feelFree:
+                "Feel free to check out the project by visiting the GitHub link.",
+            },
+            botanic: {
+              pPrimary:
+                "The Botanic project case study is presented on this page, including an overview of the project as well as the tools used.",
+              pDetail1:
+                "Botanic is a software solution developed to streamline the purchase order management process for a garden center. Built with a focus on user-friendly navigation, this application select transportation modes, and view detailed order information and delivery dates.",
+              pDetail2:
+                "Designed to simplify the purchasing workflow, Botanic enhances operational efficiency by offering an intuitive user interface and reliable data management features.",
+              pDetail3:
+                "Among its key features, Botanic provides streamlined order creation with customizable filtering options, making it easy for users to locate and manage products efficiently.",
+            },
+            galaxyRush: {
+              pPrimary:
+                "The Galaxy Rush project case study is presented on this page, including an overview of the project as well as the tools used.",
+              pDetail1:
+                "Galaxy Rush is a WPF game coded in C# with Visual Studio, designed as an endless runner where players navigate a spaceshipto dodge evolving obstacles. Inspired by Flappy Bird, players press the spacebar to change direction while collecting power-ups that grant temporary invincibility.",
+              pDetail2:
+                "As gameplay progresses, the obstacles—ranging from asteroids to UFOs—become more frequent and challenging, pushing players to survive as long as possible.",
+              pDetail3:
+                "For those curious about the game's ultimate challenge, an invincibility key allows a sneak peek at what lies further into the game.",
             },
           },
           contact: {
@@ -125,6 +147,28 @@ i18next.init(
               skills: "Compétences",
               seeGitHub: "Voir sur GitHub",
               projectsPresentaion: "Présentation du projet",
+              feelFree:
+                "N'hésitez pas à consulter le projet en visitant le lien GitHub.",
+            },
+            botanic: {
+              pPrimary:
+                "L'étude de cas du projet Botanic est présentée sur cette page, comprenant un aperçu du projet ainsi que les outils utilisés.",
+              pDetail1:
+                "Botanic est une solution logicielle développée pour rationaliser le processus de gestion des commandes d'achat pour une jardinerie. Construite avec un accent sur la navigation conviviale, cette application permet de sélectionner les modes de transport, et de visualiser les informations détaillées de la commande et les dates de livraison.",
+              pDetail2:
+                "Conçu pour simplifier le flux de travail d'achat, Botanic améliore l'efficacité opérationnelle en offrant une interface utilisateur intuitive et des fonctionnalités de gestion de données fiables.",
+              pDetail3:
+                "Parmi ses principales fonctionnalités, Botanic offre une création de commande simplifiée avec des options de filtrage personnalisables, facilitant la localisation et la gestion efficace des produits par les utilisateurs.",
+            },
+            galaxyRush: {
+              pPrimary:
+                "L'étude de cas du projet Galaxy Rush est présentée sur cette page, comprenant un aperçu du projet ainsi que les outils utilisés.",
+              pDetail1:
+                "Galaxy Rush est un jeu WPF codé en C# avec Visual Studio, conçu comme un runner infini où les joueurs naviguent dans un vaisseau spatial pour éviter des obstacles évolutifs. Inspiré de Flappy Bird, les joueurs appuient sur la barre d'espace pour changer de direction tout en collectant des power-ups qui accordent une invincibilité temporaire.",
+              pDetail2:
+                "Au fur et à mesure que le gameplay progresse, les obstacles - allant des astéroïdes aux OVNI - deviennent plus fréquents et difficiles, poussant les joueurs à survivre le plus longtemps possible.",
+              pDetail3:
+                "Pour ceux qui sont curieux du défi ultime du jeu, une touche d'invincibilité permet de jeter un coup d'œil à ce qui se trouve plus loin dans le jeu.",
             },
           },
           contact: {
